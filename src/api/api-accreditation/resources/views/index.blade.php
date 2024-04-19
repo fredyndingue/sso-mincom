52c2abecc2348c72dca5886e6f9b56b77799dac8
unknown <florentlibang@gmail.com>
2024-04-19 12:09:34 +0100

    Implement home page for accreditation's API

---
+++ src/api/api-accreditation/resources/views/index.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        h1{
+            color: rgb(197, 14, 91);
+            width: 100%;
+            height: 70vh;
+            align-items: center;
+            text-shadow: 1px 1px 5px black;
+            font-size: 5rem;
+            text-align: center;
+        }
+        h1{
+            animation: colors 5s ease-in-out infinite;
+        }
+        @keyframes colors{
+            0%{color: aqua;}
+            40%{color: black;}
+            75%{color: green;}
+            100%{color: blue;}
+        }
+        /*.defilement h1{
+            animation: defile 7s linear infinite;
+            white-space: nowrap;
+        }
+        @keyframes defile{
+            from{transform: translateX(100%);}
+            to{transform: translateX(-100%);}
+        }*/ 
+        
+    </style>
+</head>
+<body background="images/mincom.jpg">
+    <div class="defilement">
+  <h1> Bienvenue sur la page accréditation du ministère de la communication</h1>
+</div>
+</body>
+</html>
